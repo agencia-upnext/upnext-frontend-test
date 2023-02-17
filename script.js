0b807c7996a5b0ca7d01f25f897373942333d32e
Higor Rodrigues <higor_1992@hotmail.com>
2023-02-17 11:59:45 -0300

    configurado o script dos filters

--- script.js
+++ script.js
@@ -1,60 +1,42 @@
-const plantData = [
-  {
-    name: "Euphorbia eritrea",
-    sun: "high",
-    water: "rarely",
-    toxicity: false,
-  },
-  {
-    name: "Succulent Bowl",
-    sun: "high",
-    water: "rarely",
-    toxicity: false,
-  },
-  {
-    name: "Bunny ears cacti",
-    sun: "high",
-    water: "rarely",
-    toxicity: false,
-  },
-  {
-    name: "Ficus lyrata",
-    sun: "high",
-    water: "regularly",
-    toxicity: false,
-  },
-  {
-    name: "Bamboo",
-    sun: "low",
-    water: "regularly",
-    toxicity: false,
-  },
-  {
-    name: "Ponytail Palm",
-    sun: "low",
-    water: "regularly",
-    toxicity: false,
-  },
-  {
-    name: "Pilea peperomioides",
-    sun: "no",
-    water: "regularly",
-    toxicity: true,
-  },
-  {
-    name: "Calathea triostar",
-    sun: "no",
-    water: "daily",
-    toxicity: true,
-  },
-  {
-    name: "Monstera deliciosa",
-    sun: "no",
-    water: "daily",
-    toxicity: true,
-  },
-];
+let plantData = [];
+let filteredPlants = [];
 
+fetch("plants.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // Itera sobre os objetos no arquivo JSON
+    plantData = data;
+    data.forEach((plant) => {
+      // Extrai as propriedades desejadas
+      const imageUrl = plant.url;
+      const name = plant.name;
+      const price = plant.price;
+      const cart = document.createElement("li");
+
+      // Cria uma nova linha na tabela HTML com as informações extraídas
+      const row = document.createElement("ul");
+      const imageCell = document.createElement("img");
+      const nameCell = document.createElement("p");
+      const priceCell = document.createElement("p");
+
+      imageCell.src = imageUrl;
+      nameCell.innerText = name;
+      priceCell.innerText = price;
+
+      cart.appendChild(imageCell);
+      cart.appendChild(nameCell);
+      cart.appendChild(priceCell);
+      row.appendChild(cart);
+
+      row.setAttribute("class", "plant-list");
+      document.querySelector("#plantsTable tbody").appendChild(row);
+    });
+  })
+  .catch((error) => {
+    console.error("Erro ao buscar o arquivo JSON:", error);
+  });
+
+// Filtro ---
 const sunlightSelect = document.getElementById("sunlight");
 const waterSelect = document.getElementById("water");
 const petsSelect = document.getElementById("pets");
@@ -63,53 +45,56 @@ const plantList = document.getElementById("plant-list");
 function updatePlantList() {
   plantList.innerHTML = "";
 
-  const selectedSunlight = sunlightSelect.value;
-  const selectedWater = waterSelect.value;
-  const selectedPets = petsSelect.value;
-
-  const filteredPlants = plantData.filter((plant) => {
-    if (selectedSunlight && plant.sun !== selectedSunlight) {
-      return false;
-    }
-    if (selectedWater && plant.water !== selectedWater) {
-      return false;
-    }
-    if (selectedPets === "true" && plant.toxicity !== false) {
-      return false;
-    }
-    return true;
-  });
-
   filteredPlants.forEach((plant) => {
-    const li = document.createElement("li");
-    li.textContent = plant.name;
-    plantList.appendChild(li);
+    const cart = document.createElement("li");
+    const name = plant.name;
+    const price = plant.price;
+    const imageUrl = plant.url;
+    const row = document.createElement("ul");
+    const nameCell = document.createElement("p");
+    const priceCell = document.createElement("p");
+    const imageCell = document.createElement("img");
+
+    nameCell.innerText = name;
+    priceCell.innerText = price;
+    imageCell.src = imageUrl;
+
+    cart.appendChild(nameCell);
+    cart.appendChild(priceCell);
+    cart.appendChild(imageCell);
+    row.appendChild(cart);
+
+    row.setAttribute("class", "plant-list");
+    document.querySelector("#plantsTable tbody").appendChild(row);
   });
 }
 
 sunlightSelect.addEventListener("change", () => {
+  const selectedSunlight = sunlightSelect.value;
+  document.querySelector("#plantsTable tbody").innerHTML = "";
+  filteredPlants = plantData.filter((plant) => plant.sun === selectedSunlight);
   updatePlantList();
+  petsSelect.value = "";
+  waterSelect.value = "";
 });
 
 waterSelect.addEventListener("change", () => {
+  const selectedWater = waterSelect.value;
+  document.querySelector("#plantsTable tbody").innerHTML = "";
+  filteredPlants = plantData.filter((plant) => plant.water === selectedWater);
   updatePlantList();
+  sunlightSelect.value = "";
+  petsSelect.value = "";
 });
 
 petsSelect.addEventListener("change", () => {
-  if (petsSelect.value === "true") {
-    const filteredPlants = plantData.filter(
-      (plant) => plant.toxicity === false
-    );
-
-    sunlightSelect.value = "";
-    waterSelect.value = "";
-
-    filteredPlants.forEach((plant) => {
-      const li = document.createElement("li");
-      li.textContent = plant.name;
-      plantList.appendChild(li);
-    });
-  } else {
-    updatePlantList();
-  }
+  const selectedPets = petsSelect.value;
+  const selectIsTrue = selectedPets === "true";
+  document.querySelector("#plantsTable tbody").innerHTML = "";
+  filteredPlants = plantData.filter((plant) => {
+    return plant.toxicity !== selectIsTrue;
+  });
+  updatePlantList();
+  sunlightSelect.value = "";
+  waterSelect.value = "";
 });
